455f86239990c51995dab9f128924e05c397d79a
Ogoh Samuel <84603880+Samkingworld@users.noreply.github.com>
2022-07-03 03:56:43 +0100

    Join-community

---
+++ script.js
@@ -0,0 +1,13 @@
+ let signup = document.querySelector(".signup");
+ let popup = 
+ 				
+ document.querySelector(".bg-model");
+ let close = document.querySelector(".close");
+ signup.addEventListener("click", function () {
+				popup.style.display = "flex";
+});
+
+
+close.addEventListener("click", function () {
+				popup.style.display = "none";
+});
